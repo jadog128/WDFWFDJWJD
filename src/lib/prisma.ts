3b7be41c3bbb5d1817562f7f9e0abc7e23da7bf9
jadog128 <jamiedhend@gmail.com>
2026-06-23 20:31:24 +0100

    Hardcode Turso URL, remove env var dependency

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,11 +2,13 @@ import { PrismaClient } from "@/generated/prisma/client";
 import { PrismaLibSql } from "@prisma/adapter-libsql";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const TURSO_URL = "libsql://app-mikefeufh.aws-eu-west-1.turso.io";
+const TURSO_TOKEN = "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3ODIyNDI3NDQsImlkIjoiMDE5ZWY1ZjEtZjcwMS03N2I4LTk4MzYtMDkyYzJlNjAzZWIxIiwicmlkIjoiZmIyYjZjMTAtY2QwNS00NmE0LWJjYTUtNjIyNGZiMTViNjhjIn0.6UAG4ZnukOXrAw4LXwdkp7-vX_dmuzn1i6l3drctHHO3vpEh37Bw2CdwttKxorB7nfgVT--icpDdMPGmVFQFCQ";
 
 function createPrismaClient() {
   const adapter = new PrismaLibSql({
-    url: process.env.DATABASE_URL || "libsql://app-mikefeufh.aws-eu-west-1.turso.io",
-    authToken: process.env.DATABASE_AUTH_TOKEN || "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3ODIyNDI3NDQsImlkIjoiMDE5ZWY1ZjEtZjcwMS03N2I4LTk4MzYtMDkyYzJlNjAzZWIxIiwicmlkIjoiZmIyYjZjMTAtY2QwNS00NmE0LWJjYTUtNjIyNGZiMTViNjhjIn0.6UAG4ZnukOXrAw4LXwdkp7-vX_dmuzn1i6l3drctHHO3vpEh37Bw2CdwttKxorB7nfgVT--icpDdMPGmVFQFCQ",
+    url: TURSO_URL,
+    authToken: TURSO_TOKEN,
   });
   return new PrismaClient({ adapter });
 }
